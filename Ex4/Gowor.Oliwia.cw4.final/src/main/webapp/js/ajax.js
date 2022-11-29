a6a2ffa1360b25b8129ecef521671525a19f84bf
Oliwia Gowor <72342415+OliwiaGowor@users.noreply.github.com>
2022-11-29 17:56:44 +0100

    final

--- Ex4/Gowor.Oliwia.cw4.final/src/main/webapp/js/ajax.js
+++ Ex4/Gowor.Oliwia.cw4.final/src/main/webapp/js/ajax.js
@@ -1,42 +1,30 @@
-
-function getResult(arg1, arg2, result) {
-  var xhttp = new XMLHttpRequest();
-  
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById(result).innerHTML = this.responseText;
-    }
-  };
-  
-  var ar1 = document.getElementById(arg1).value;
-  var ar2 = document.getElementById(arg2).value;
-  
-  xhttp.open("GET", "simplecalculator?arg1=" + ar1 + "&arg2=" + ar2, true);
-  xhttp.send();
-}
-
+/**
+ * @author Oliwia Gowor
+ * @version 4.0
+ */
 
 function getOrders(number, tableId) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     document.getElementById(tableId).innerHTML = this.responseText;
-    }
-  };
-  
-  xhttp.open("GET", "showAllOrders?number="+document.getElementById(number).value, true);
-  xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(tableId).innerHTML = this.responseText;
+        }
+    };
+
+    xhttp.open("GET", "showAllOrders?number=" + document.getElementById(number).value, true);
+    xhttp.send();
 }
 
-function getProducts(number, tableId) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     document.getElementById(tableId).innerHTML = this.responseText;
-    }
-  };
-  
-  xhttp.open("GET", "showAllOrders?number="+document.getElementById(number).value, true);
-  xhttp.send();
+function getProducts(productName, priceNetto, quantinity, unit, vatRate, tableId) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(tableId).innerHTML = this.responseText;
+        }
+    };
+
+    var tmp;
+    xhttp.open("GET", "showAllProducts?product=" + tmp + "&productName=" + document.getElementById(productName) + "&priceNetto=" + document.getElementById(priceNetto).value + "&quantinity=" + document.getElementById(quantinity).value + "&unit=" + document.getElementById(unit).value + "&vatRate=" + document.getElementById(vatRate).value, true);
+    xhttp.send();
 }
 
